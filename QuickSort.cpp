9da83c7cb3b85d2cc860d3cde79e5a49981fc92a
AYMERIC-MSI\Aymeric <xhin_12@hotmail.fr>
2018-09-11 10:18:08 +0200

    fix

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -4,83 +4,95 @@
 #include <chrono>
 #include <iomanip>
 
-void TestQuickSort(int size);
-int Partition(int* pArray, int lo, int hi);
-void QuickSort(int* pArray, int lo, int hi);
-void Swap(int* pArray, int a, int b);
+void TestQuickSort( int size );
+int Partition( int* pArray, int lo, int hi );
+void QuickSort( int* pArray, int lo, int hi );
+void Swap( int* pArray, int a, int b );
+void PrintArray( int* pArray, const int size );
 
 int main()
 {
-	srand(unsigned int(time(nullptr)));
+	srand( unsigned int( time( nullptr ) ) );
 	const int startSize{ 1 };
 	const int maxSize{ 1024 };
 	const int sizeMultiplier{ 2 };
 	std::cout << "--- Test Quick Sort ---\n\n";
 	std::cout << "Size\tAverage time\n";
-	for (int i{ startSize }; i <= 1024; i *= sizeMultiplier)
+	for( int i{ startSize }; i <= maxSize; i *= sizeMultiplier )
 	{
-		TestQuickSort(i);
+		TestQuickSort( i );
 	}
 	std::cout << "\n\nPress ENTER to quit ";
 	std::cin.get();
-    return 0;
+	return 0;
 }
 
-void TestQuickSort(const int size)
+void TestQuickSort( const int size )
 {
 	const int iterations{ 100 };
 	double averageTime{};
 
-	for (int i{ 0 }; i < iterations; ++i)
+	for( int i{ 0 }; i < iterations; ++i )
 	{
 		int* pArray{ new int[size]() };
-		for (int i{ 0 }; i < size; ++i)
+		for( int i{ 0 }; i < size; ++i )
 		{
 			pArray[i] = rand() % RAND_MAX;
 		}
 
 		auto start = std::chrono::steady_clock::now();
-		QuickSort(pArray, 0, size - 1);
+		//PrintArray( pArray, size );
+		QuickSort( pArray, 0, size - 1 );
+		//PrintArray( pArray, size );
 		auto end = std::chrono::steady_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start );
 		averageTime += duration.count();
 	}
 	averageTime /= iterations;
-	std::cout << size << '\t' << std::fixed << std::setprecision(16) << averageTime << '\n';
+	std::cout << size << '\t' << std::fixed << std::setprecision( 16 ) << averageTime << '\n';
 }
 
-int Partition(int* pArray, int lo, int hi)
+int Partition( int* pArray, int lo, int hi )
 {
 	int pivot{ pArray[hi] };
 	int i{ lo - 1 };
-	for (int j{ lo }; j < hi; ++j)
+	for( int j{ lo }; j < hi; ++j )
 	{
-		if (pArray[j] < pivot)
+		if( pArray[j] < pivot )
 		{
 			++i;
-			if (pArray[i] != pArray[j])
+			if( pArray[i] != pArray[j] )
 			{
-				std::swap(pArray[i], pArray[j]);
+				Swap( pArray, i, j );
 			}
 		}
 	}
-	std::swap(pArray[i+1], pArray[hi]);
+	Swap( pArray, i + 1, hi );
 	return i + 1;
 }
 
-void QuickSort(int* pArray, int lo, int hi)
+void QuickSort( int* pArray, int lo, int hi )
 {
-	if (lo < hi)
+	if( lo < hi )
 	{
-		int pivot{ Partition(pArray, lo, hi) };
-		QuickSort(pArray, lo, pivot - 1);
-		QuickSort(pArray, pivot + 1, hi);
+		int pivot{ Partition( pArray, lo, hi ) };
+		QuickSort( pArray, lo, pivot - 1 );
+		QuickSort( pArray, pivot + 1, hi );
 	}
 }
 
-void Swap(int* pArray, int a, int b)
+void Swap( int* pArray, int a, int b )
 {
 	int holder{ pArray[a] };
 	pArray[a] = pArray[b];
 	pArray[b] = holder;
+}
+
+void PrintArray( int* pArray, const int size )
+{
+	for( int i{ 0 }; i < size; ++i )
+	{
+		std::cout << pArray[i] << ' ';
+	}
+	std::cout << '\n';
 }
